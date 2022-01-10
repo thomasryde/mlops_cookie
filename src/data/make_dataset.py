62f6dfd336849eff370a3130aaabe6e4d0f39efd
thomasryde <thomas.ryde@gmail.com>
2022-01-10 15:18:54 +0100

    remove shit

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -48,7 +48,6 @@ def main(input_filepath, output_filepath):
 
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
-hwa = 2
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
